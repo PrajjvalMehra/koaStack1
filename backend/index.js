e876893fb55e7a505bcdaef0a17b5845d67df96d
PrajjvalMehra <mehraprajjval@gmail.com>
2021-05-20 11:25:57 +0530

    backendfinal

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,94 @@
-const Koa = require ('Koa');
-require("dotenv").config();
+var koa = require('koa');
+var router = require('koa-router')();
+var app = new koa();
+var mongoose = require('mongoose');
+var koaBody = require('koa-body');
 
-const app = new Koa();
-const port = process.env.PORT || 8080;
 
-require("./src/app/config/mongo");
-require("./src/app/router")(app);
 
-console.log("port",port);
+mongoose.connect(
+    "mongodb://localhost:27017/koaCrud",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    () => {
+      console.log("mongo connection successful");
+    }
+  );
 
-app.use(ctx=>{
-    ctx.body = "welcome to koa"
+var schema = mongoose.Schema({
+    name:String,
+    age:Number,
+    phone:String
+})
+
+var User = mongoose.model("User",schema);
+
+
+// app.use(async ctx => {
+//     ctx.body = 'Hello World';
+//   });
+
+  async function  getUser(){
+        var user = await User.find()
+        
+        return user
+    }
+
+
+
+
+router.get('/user',async ( ctx,next)=>{
+    var response  = await getUser();
+    ctx.body = response
+    console.log (response)
+    // ctx.body = "hello"
 });
 
+ router.post('/user',async (ctx,next)=>{
+        
+        var body =  await ctx.request.body;
+        var user = new User();
 
-app.listen(port, () => {
-    console.log(`Server started at ${port}`)
+        user.name = body.name;
+        user.age = body.age;
+        user.phone = body.phone;
+        user.save();
+
+        ctx.body = {user:user}
+    
+   
+    
+});
+
+router.put('/user/:id', async(ctx,next)=>{
+    
+        var _id = ctx.request.params.id;
+        
+        var updateUser = await User.findByIdAndUpdate(_id, ctx.request.body)
+        ctx.body={updateUser}
+    
+    
+    
 })
+
+router.delete('/user/:id', async(ctx,next)=>{
+    var _id = ctx.request.params.id;
+
+    var deleteUser = await User.findByIdAndRemove(_id)
+    ctx.body = {deleteUser}
+})
+    
+
+
+
+
+
+app.use(koaBody({ multipart: true }))
+
+app
+.use(router.routes())
+.use(router.allowedMethods());
+app.listen(3000);
+
--- backend/src/app/controller/UserController.js
+++
@@ -1,7 +0,0 @@
-const model = require("../model/User");
-
-exports.getUser  = async ( ctx,next )=>{
-    var response = await model.getUser();
-    ctx.body = response;
-    console.log("controller")
-}
